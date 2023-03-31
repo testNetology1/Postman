28d810f574ed3e254e4b20773d0936506951c5f3
Albina <latipova.alb@gmail.com>
2023-03-31 16:19:15 +0500

    postman

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,21 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    public void shouldPostToPostmanEcho() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(" data"))
+        ;
+    }
+}
