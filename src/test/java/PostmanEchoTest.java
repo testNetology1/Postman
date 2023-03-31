146922b695e121ae43b6d0a947b7a45c9187a3a0
Albina <latipova.alb@gmail.com>
2023-03-31 16:23:12 +0500

    test postman

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -15,7 +15,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo(" data"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
